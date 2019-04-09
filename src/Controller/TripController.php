01bd7c847e6558c38d7116dfafee891b81074823
Vladimir Zakotnev <vladitot@gmail.com>
2019-04-09 16:39:37 +0300

    Readme + мелкие правки роутов

--- src/Controller/TripController.php
+++ src/Controller/TripController.php
@@ -13,7 +13,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/trip")
+ * @Route("/")
  */
 class TripController extends AbstractController
 {
